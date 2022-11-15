2678bcb29c0ae8b6ec4d2d94c1950cf5706d9e32
tajrink <tajrinkashem@gmail.com>
2022-11-14 20:27:00 -0500

    revised

--- analysis.py
+++ analysis.py
@@ -1,15 +1,9 @@
 import csv
 
-#def get_highest_gdp(data, year):`  
- # This function will return the name of the state had the highest GDP for a specified year. It takes in a string `year` as an argument. `data` will be a `DictReader` object containing data.  
-
-#`def get_lowest_gdp(data, year):`  
- # This function will return the name of the state had the lowest GDP for a specified year. It takes in a string `year` as an argument. `data` will be a `DictReader` object containing data.  
-
-#def get_state_gdp(data, state, year):`  
- # This function will return gdp value of some specific state for some specific year column. The name of the state will be passed in `state`, and the year will be passed in via `year`.
-
 def get_highest_gdp(data, year):
+    # This function will return the name of the state had the highest GDP for a specified year. 
+    # It takes in a string `year` as an argument. `data` will be a `DictReader` object containing data.  
+
     state=data[0]["GeoName"]
     max_gdp=float(data[0][year])
     
@@ -21,6 +15,9 @@ def get_highest_gdp(data, year):
     return state
 
 def get_lowest_gdp(data, year):
+    # This function will return the name of the state had the lowest GDP for a specified year.
+    # It takes in a string `year` as an argument. `data` will be a `DictReader` object containing data.  
+
     state=data[0]["GeoName"]
     min_gdp=float(data[0][year])
     
@@ -32,11 +29,13 @@ def get_lowest_gdp(data, year):
     return state
 
 def get_state_gdp(data, state, year):
+    # This function will return gdp value of some specific state for some specific year column. 
+    # The name of the state will be passed in `state`, and the year will be passed in via `year`.
     for row in data:
         if state==row["GeoName"]:
             return row[year]
 
-#this appends all rows as dictionaries to list_data
+
 # save each row into a list (TODO: change to your path!)
 list_data = []
 with open("state_gdp_analysis.csv", "r") as infile:
@@ -55,5 +54,5 @@ print(highest,lowest)
 prev = get_state_gdp(list_data, "New York", "2019")
 new = get_state_gdp(list_data, "New York", "2020")
 
-#print(prev,new)
+print(prev,new)
 print(float(new) - float(prev))
